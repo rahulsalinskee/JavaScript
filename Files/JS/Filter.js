444dc8e4ea23c4e7b2b2c9f5236305a2e79d448c
rahul <rahulsalinskee@gmail.com>
2024-10-22 19:13:11 +0530

    Filter

---
+++ Files/JS/Filter.js
@@ -0,0 +1,35 @@
+/* ***** Filter Number Greater Than 30 from Number Array using Arrow Function & Filter (With double return statement && extra brace) ***** */
+{
+    /**
+     * Returns an array of numbers from the given array of numbers that are greater than 30.
+     *
+     * @param {number[]} numbers - An array of numbers.
+     * @returns {number[]} An array of numbers greater than 30 from the given array of numbers.
+     */
+    let GetNumberGreaterThanThirty = (numbers) => {
+        return numbers.filter(number => {
+            return number > 30;
+        });
+    };
+
+    let numbers = [20, 22, 44, 66, 98, 30, 84, 10, 12];
+    let numberGreaterThanThirty = GetNumberGreaterThanThirty(numbers);
+    console.log(`Number Greater Than 30: ${numberGreaterThanThirty}`);
+}
+
+/* ***** Filter Number Greater Than 30 from Number Array using Arrow Function & Filter (With single return statement && no extra brace) ***** */
+{
+    /**
+     * Returns an array of numbers from the given array of numbers that are greater than 30.
+     *
+     * @param {number[]} numbers - An array of numbers.
+     * @returns {number[]} An array of numbers greater than 30 from the given array of numbers.
+     */
+    let GetNumberGreaterThanThirty = (numbers) => {
+        return numbers.filter(number => number > 30);
+    }
+
+    let numbers = [20, 22, 44, 66, 98, 30, 84, 10, 12];
+    let numberGreaterThanThirty = GetNumberGreaterThanThirty(numbers);
+    console.log(`Number Greater Than 30: ${numberGreaterThanThirty}`);
+}
