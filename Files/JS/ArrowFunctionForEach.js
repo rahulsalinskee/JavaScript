47f3bd5e8ae47b10f5cf4f745af339db478f70bb
rahul <rahulsalinskee@gmail.com>
2024-10-22 16:59:54 +0530

    Normal Function and Arrow Function

---
+++ Files/JS/ArrowFunctionForEach.js
@@ -0,0 +1,52 @@
+
+/* ***** Get Value Count From any Entered Text using Normal Function & Foreach ***** */
+{
+    let text = prompt("Enter Text");
+    let VowelCount = GetVowelCount(text);
+    console.log(`Vowel Count (Normal Function): ${VowelCount}`);
+
+    /**
+     * Counts the number of vowels in a given text.
+     *
+     * @param {string} text the text to count the vowels for
+     * @returns {number} the number of vowels in the given text
+     */
+    function GetVowelCount(text) {
+        let count = 0;
+        const vowels = ['a', 'e', 'i', 'o', 'u'];
+        vowels.forEach(vowel => {
+            for (let i = 0; i <= text.length; i++) {
+                if (vowel === text.charAt(i).toLowerCase()) {
+                    count++;
+                }
+            }
+        })
+        return count;
+    }
+}
+
+/* ***** Get Value Count From any Entered Text using Arrow Function & Foreach ***** */
+{
+    /**
+     * Counts the number of vowels in a given text.
+     *
+     * @param {string} text the text to count the vowels for
+     * @returns {number} the number of vowels in the given text
+     */
+    const GetVowelCount = (text) => {
+        let count = 0;
+        const vowels = ['a', 'e', 'i', 'o', 'u'];
+        vowels.forEach(v => {
+            for (let i = 0; i <= text.length; i++) {
+                if (v === text.charAt(i).toLowerCase()) {
+                    count++;
+                }
+            }
+        })
+        return count;
+    }
+
+    let text = prompt("Enter Text");
+    let vowelCount = GetVowelCount(text);
+    console.log(`Vowel Count (Arrow Function): ${vowelCount}`);
+}
